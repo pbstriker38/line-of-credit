ad371255bcb19a0235680082171c9b5bd1623551
Daniel Alfaro <daniel.alfaro@reachlocal.com>
2015-12-17 20:19:41 -0800

    Add a method that will be run at the end of a period - This will calculate the total interest owed for the period and  update the balance with that interest. It will also set the next  periods start date.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   include Clearance::User
+  include InterestHelper
 
   monetize :credit_limit_cents
   monetize :balance_cents
@@ -20,6 +21,35 @@ class User < ActiveRecord::Base
   def update_balance(amount)
     self.credit_limit += amount
     self.balance += amount
+    self.save
+  end
+
+  def close_period
+    calculate_interest(self)
+
+    # interest_for_period is made negative because it is money owed.
+    total_interest = -interest_for_period
+    Transaction.create(amount: total_interest, transaction_type: 'period interest', user_id: self.id)
+
+    update_balance(total_interest)
+    reset_period_start
+  end
+
+  def interest_for_period
+    interest_transactions = transactions
+                            .where("created_at >= ?", self.period_start )
+                            .where(transaction_type: ['interest'])
+
+    interest = Money.new(0)
+    interest_transactions.each do |transaction|
+      interest += transaction.amount
+    end
+
+    return interest
+  end
+
+  def reset_period_start
+    self.period_start = Time.zone.now.beginning_of_day
     self.save
   end
 
